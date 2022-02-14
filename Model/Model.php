58670358f063fd565bf33e4e02fbf633f8814212
yacine-boucetta <yacine.boucetta@laplateforme.io>
2022-02-14 10:51:53 +0100

    insertion des fonction

--- Model/Model.php
+++ Model/Model.php
@@ -1,30 +1,45 @@
 <?php
+session_start();
 
+class Model
+{
 
+    private $dbName;
+    private $hostName;
+    private $dbLogin;
+    private $dbPassword;
 
-class Model{
+    function __construct()
+    {
+        $this->dbName = 'boutique';
+        $this->hostName = 'localhost';
+        $this->dblogin = 'root';
+        $this->dbPassword = 'root';
 
-private $dbName;
-private $hostName;
-private $dbLogin;
-private $dbPassword;
-
-    function __construct(){
-        $this->dbName ='boutique';
-        $this->hostName ='localhost';
-        $this->dblogin='root';
-        $this->dbPassword='root';
-        $this->db='';
-    try{
-
-    $this->db= new PDO('mysql:host='.$this->hostName.';dbname='.$this->dbName.';charset=utf8,'.$this->dbLogin.','.$this->dbPassword.'');
+        try {
+            $this->db = new PDO('mysql:host=' . $this->hostName . ';dbname=' . $this->dbName . ';charset=utf8,' . $this->dbLogin . ',' . $this->dbPassword . '');
+        } catch (PDOException $e) {
+            echo 'Echec de la connexion : ' . $e->getMessage();
+            exit;
+        }
     }
 
-    catch (PDOException $e) {
-        echo 'Echec de la connexion : ' . $e->getMessage();
-        exit;
+    public  function checkUser($login)
+    {
+        $sqlinsert = "SELECT * FROM utilisateurs WHERE login=:login ";
+        $signIn = $this->db->prepare($sqlinsert);
+        $signIn->execute(array(
+            ':login' => $login
+        ));
+        $checkUser = $signIn->fetch(PDO::FETCH_ASSOC);
+        return $checkUser;
     }
-}
 
-
+    public function getProd()
+    {
+        $getProd = $this->db->prepare("SELECT * FROM product");
+        $getProd->execute();
+        $getProduit = $getProd->fetchall(PDO::FETCH_ASSOC);
+        return $getProduit;
+    }
 }
