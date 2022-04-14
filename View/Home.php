6b272f549c532f8456fce5927dcbec64d8e4d6ef
thomas-dellacase <thomas.dellacase@laplateforme.io>
2022-04-14 09:54:42 +0200

    ok

--- View/Home.php
+++ View/Home.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <!-- <link rel="stylesheet" href="./assets/css/home.css"> -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/homeMobile.css">
@@ -33,41 +33,28 @@
     </div>
   </div>
 </nav>
-
-        <!-- <nav class="navbar">
-            <h1>GASHIDO</h1>
-            <ul>
-                <li><a href='./SignUp'>inscription</a></li>
-                <li><a href='./SignIn'>connexion</a></li>
-                <li><a href='./Product'>product</a></li>
-                <li><a href='./Admin'>admin</a></li>
-                <li><a href='./Profil'>profil</a></li>
-                <li><a href='./Deconnexion'>deconnexion</a></li>
-            </ul>
-            <form action="" method="post" class="search">
-                <input type="text" name="query" />
-                <input type="submit" value="Search" />
-            </form>
-
-        </nav> -->
     </header>
     <div class="border">
-        <section class="produit">
-            <h2>LES COUTEAUX</h2>
-        </section>
-        <main class="home">
-            <div class="couteau">
-                <a href="./Product">
-                    <h2>l'atelier</h2>
-                </a>
-            </div>
-            <div class="accessoire">
-                <a href="'./Product'">
-                    <h2>accessoire</h2>
-                </a>
-            </div>
-        </main>
-    </div>
+    <section class="produit">
+        <h2>LES Produits</h2>
+    </section>
+    <main class="home">
+
+        <div class="couteau">
+            <h2 >LE COUTAL</h2>
+            <a href='./Product'>
+                <img src="./assets/images/gashidopattern (3).png" alt="img"></img>
+            </a>
+        </div>
+
+        <div class="accessoire">
+            <h2>Accessoire</h2>
+            <a href=/Product>
+            <img src="./assets/images/gashidopattern (3).png" alt="img"></img>
+            </a>
+        </div>
+    </main>
+
     <footer>
 
     </footer>
