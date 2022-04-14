e4674a975828fdf5ec049c9378306dd5cf6471b9
thomas-dellacase <thomas.dellacase@laplateforme.io>
2022-04-14 08:27:01 +0200

    ok

---
+++ View/404.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- <link rel="stylesheet" href="./assets/css/home.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/homeMobile.css">
+    <title>Document</title>
+</head>
+<?php ?>
+
+<body>
+    <header style="width:100%;">
+    <nav class="navbar navbar-expand-lg navbar-light " style="background-color:#2c2c2c;" >
+        <a class="" href="#">GASHIDO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="" href="#">inscription <span class="sr-only">(current)</span></a>
+            <a class="" href="#">connexion</a>
+            <a class="" href="#">product</a>
+            <a class="" href="#">admin</a>
+            <a class="" href="#">profil</a>
+            <a class="" href="#">deconnexion</a>
+        </div>
+    </div>
+    </nav>
+    <main>
+        <div id="page404">
+
+        </div>
+    </main>
+</body>
--- View/Admin.php
+++ View/Admin.php
@@ -1,31 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- <link rel="stylesheet" href="./assets/css/home.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/admin.css">
     <title>Document</title>
 </head>
+<?php ?>
+
 <body>
     <header>
-        <nav class="navbar">
-            <h1> GASHIDO </h1>
-            <ul>
-                <li><a href='./SignUp'>inscription</a></li>
-                <li><a href='./SignIn'>connexion</a></li>
-                <li><a href='./Product'>product</a></li>
-                <li><a href='./Admin'>admin</a></li>
-                <li><a href='./Profil'>profil</a></li>
-                <li><a href='./Deconnexion'>deconnexion</a></li>
-            </ul>
-            <form action="" method="post">
-	            <input type="text" name="query" />
-	            <input type="submit" value="Search" />
-            </form>
-        </nav>
+    <nav class="navbar navbar-expand-lg navbar-light " style="background-color:#2c2c2c;" >
+  <a class="" href="#">GASHIDO</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="" href="#">inscription <span class="sr-only">(current)</span></a>
+      <a class="" href="#">connexion</a>
+      <a class="" href="#">product</a>
+      <a class="" href="#">admin</a>
+      <a class="" href="#">profil</a>
+      <a class=" href="#">deconnexion</a>
+    </div>
+  </div>
+</nav>
     </header>
-<body>
     <main>
     <h2 id='admin'>ADMIN</h2>
     <h3 style="color:red"><?php echo $message; ?></h3>
@@ -148,7 +156,7 @@
                             $option = new Admin();
                             $option->prodSelect();
                         ?>
-                </select>
+                    </select>
                 </div>
                 <input class='inputProd' type='text' name='nom' placeholder='Nouveau nom de produit'></input>
                 <textarea class='inputProd' name='upDescription' style="resize: none;" placeholder='Nouvelle description du produit'></textarea>             
